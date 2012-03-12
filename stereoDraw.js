db4e6327f4eda70f53db448c0884b468baa69924
Kevin Tong <logicmd@gmail.com>
2012-03-12 22:37:59 +0800

    update license

--- stereoDraw.js
+++ stereoDraw.js
@@ -1,17 +1,22 @@
 ﻿/*
+ *  HTML5 Anaglyph Image
+ * 
+ *  Copyright (C) 2012 Kevin Tong
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-HTML5 Stereo Viewer
-
-version 1.3 Simplifed
-
-Copyright (C) 2011 Yury Golubinsky mod by logicmd
-
-This work is licensed under the
-Creative Commons Attribution 3.0 Unported License.
-To view a copy of this license,
-visit http://creativecommons.org/licenses/by/3.0/
-
-*/
 
 function stereoDrawImage(imgSrc, stereoType, anaglyphMode, glassType, scaleRate, cnvsDstID) {
 	
